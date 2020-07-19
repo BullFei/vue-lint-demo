2fdbf6b4577b4a3f1e7b6ba3433ece5ae76c6fee
码农雨飞 <tian18840876830@163.com>
2020-07-19 17:41:31 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+/* eslint-disable*/
+//以下的所有代码就不做代码检查了
+const age = 10
+if (age == '10') {
+  console.log('goods')
+}
+
 new Vue({
   render: h => h(App)
 }).$mount('#app')
